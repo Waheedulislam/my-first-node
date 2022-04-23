14cc17987e4e0532aac5464dd090129aa7ea1ed5
Whaeedil <baoppyhossen1234@gmail.com>
2022-04-23 15:22:03 +0600

    stepes-added

--- _setps.js
+++ _setps.js
@@ -1,12 +1,17 @@
 /**
+ * -------------------
+ * Basic setup 
+ * -------------------
  * 1. create a folder (manually or mkdir)
  * 2. open command line to that folder 
  * 3. npm init
  * 4. npm install express
  * 5. create index.js
+ * 
  * --------------
  * For Express/node server
  * --------------
+ * 
  * 1. require express
  * 2. create app variable
  * 3. declare port
@@ -14,4 +19,18 @@
  * 5. listen to port 
  * 6. node index.js
  * 7. check your browser for that port
+ * 
+ * ---------------
+ * get
+ * ---------------
+ * 
+ * 1. cors middleware
+ * 2. npm install cors
+ * 3. const cors = require('cors');
+ * 4. app.use(cors());
+ * 
+ * ----------------
+ * post
+ * ----------------
+ * 1. 
 */ 
